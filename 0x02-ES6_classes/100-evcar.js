2421677b2439f3b6c188ce4aeb9235cf0304ac1d
adiomi90 <adiomi90@outlook.com>
2024-05-30 20:21:30 +0300

    evcar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,16 +1,14 @@
-export default class Car {
-  constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand = '', motor = '', color = '', range = '') {
+    super(brand, motor, color);
+    this._range = range;
   }
 
   static get [Symbol.species]() {
-    return this;
-  }
-
-  cloneCar() {
-    const ModelCar = this.constructor[Symbol.species];
-    return new ModelCar();
+    return Car;
   }
 }
+
+export default EVCar;
